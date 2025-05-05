b23d72d5b23bc80c6a44a74b3d8bd2fa14da2176
Adolfo <adolfolozanomendoza02@gmail.com>
2025-05-05 09:07:42 +0200

    Run 3 (cambios puertos)

--- source/web/web.js
+++ source/web/web.js
@@ -45,6 +45,6 @@ app.get('/probe', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Web service running on port 3000');
+app.listen(5000, () => {
+  console.log('Web service running on port 5000');
 });
