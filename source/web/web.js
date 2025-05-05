562ec1c5ff75527daeabbf97134b1ec069566cc0
Adolfo <adolfolozanomendoza02@gmail.com>
2025-05-05 09:33:26 +0200

    Run 7 (Estaba mal escrit)

--- source/web/web.js
+++ source/web/web.js
@@ -37,7 +37,7 @@ app.post('/phrase', async (req, res) => {
   }
 });
 
-app.get('/prhase', async (req, res) => {
+app.get('/phrase', async (req, res) => {
   try {
     res.send('Service is alive');
   } catch (err) {
